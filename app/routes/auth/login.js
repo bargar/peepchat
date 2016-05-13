c02cf2ba0ef8c785a86150c17f130517c61a9b7a
Jeremiah Bargar <jeremy@formigaconsulting.com>
2016-05-13 14:20:30 -0400

    full login backed by ember-simple-auth and ember-network

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route, inject } = Ember;
+
+export default Route.extend({
+  session: inject.service(),
   actions: {
     doLogin() {
-      console.log('login attempted');
-    },
+      const user = this.get('currentModel');
+      this.get('session').authenticate('authenticator:peepchat', user.email,
+        user.password);
+    }
   },
   model() {
     return {
